86aa7e579e4db8a67f63f48190e1a4613178f8ea
Daniel Duffy <dpd1208@gmail.com>
2023-06-04 09:11:38 -0600

    change to hash router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Main from "./Main";
  
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Main />
-  </BrowserRouter>,
+  </HashRouter>,
   document.querySelector("#root")
 );
